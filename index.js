a55ab9bc533fd6da10566c1d67fbde276df6fb5d
Dipali <dipalibohara500@gmail.com>
2024-03-18 05:12:13 -0700

    basic setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.use(express.json())
+
+require('dotenv').config({path:'../.env'});
+
+const port = process.env.PORT||3000;
+
+app.listen(port, ()=>{console.log(`server listen on port ${port}`)});
