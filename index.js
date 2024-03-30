3c41d7162c5d643015f7ac180edcd02135f0bc3e
Dipali <dipalibohara500@gmail.com>
2024-03-30 00:27:08 -0700

    establish a database connection

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 app.use(express.json())
-const mongoose = require('mongoose');
-
+const route = require('./router/routes');
 require('dotenv').config({path:'../.env'});
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.clusterString,).then(()=>{console.log("mongoDB connected successfully")})
+.catch((error)=>{console.log(error.message)});
+
 const port = process.env.PORT||3000;
+
+app.use('/',route);
 
 app.listen(port, ()=>{console.log(`server listen on port ${port}`)});
