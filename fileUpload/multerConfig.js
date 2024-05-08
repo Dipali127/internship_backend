a29addde0335effaded789cf5be654dde93b113d
Dipali <dipalibohara500@gmail.com>
2024-05-08 02:27:44 -0700

    change in upload file

--- fileUpload/multerConfig.js
+++ fileUpload/multerConfig.js
@@ -1,7 +1,33 @@
 const multer = require("multer");
-const upload = multer({dest: './uploaded/files'});
+//the below code doesn't provide acess of reading or manipulating file.
+//create an instance of multer
+//const upload = multer({dest: './uploaded/files'}); //automatically create folder 'uploaded' in our local system .
+
+//created multer instance with disk storage to upload files
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, './uploads') // null is custom error added by developer
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`)
+    }
+
+})
+
+// Create a Multer instance with the configured storage
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        if (file.mimetype === 'application/pdf') {
+            cb(null, true); // accept file
+        } else {
+            cb(new Error('Only pdf files are allowed'), false) //doesn't accept the file
+        }
+    }
+});
+
+// Export the Multer instance
+module.exports = upload.single("resume")
 
 
-// Export Multer middleware function
-module.exports = upload.single("resume");
 
