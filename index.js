19d5eefd28ab6b0bc671fb323fbcb59e66f504ea
Dipali <dipalibohara500@gmail.com>
2024-05-17 05:49:02 -0700

    added useful comments

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-app.use(express.json()) //handling json data
-app.use(express.urlencoded({extended:true}))  //handling form data(helps to parse the form data)
+//Middleware to handle json data
+app.use(express.json())
+// Middleware to handle URL-encoded form data
+app.use(express.urlencoded({extended:true}))  
 
 const route = require('./router/routes');
 require('dotenv').config({path:'../.env'});
 
 const mongoose = require('mongoose');
+// Connect to MongoDB using connection string from environment variables
 mongoose.connect(process.env.clusterString,).then(()=>{console.log("mongoDB connected successfully")})
 .catch((error)=>{console.log(error.message)});
 
@@ -16,6 +19,8 @@ const port = process.env.PORT||3000;
 // Any files in the 'uploads' directory can be accessed via URLs starting with '/uploads'
 app.use('/uploads', express.static('uploads'));
 
+// Use routes defined in the 'router/routes' module
 app.use('/',route);
 
+// Start the server and listen on the specified port
 app.listen(port, ()=>{console.log(`server listen on port ${port}`)});
