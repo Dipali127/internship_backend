5a9e668bc25ff4aee5435a360c47bc75243b74d0
Dipali <dipalibohara500@gmail.com>
2024-05-23 05:39:15 -0700

    add some code

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Load environment variables from .env file
 require('dotenv').config({ path: '../.env' });
+const port = process.env.PORT || 3000;
 
 //Import route
 const route = require('./router/routes');
@@ -16,8 +17,6 @@ const mongoose = require('mongoose');
 // Connect to MongoDB using connection string from environment variables
 mongoose.connect(process.env.clusterString,).then(() => { console.log("mongoDB connected successfully") })
     .catch((error) => { console.log(error.message) });
-
-const port = process.env.PORT || 3000;
 
 // Use routes defined in the 'router/routes' module
 app.use('/', route);
